1f39d628e197e6e6829cb44993df683eaea80546
Friczh <111292694+Friczh@users.noreply.github.com>
2026-06-21 07:59:19 +0700

    Update LM.user.js

--- LM.user.js
+++ LM.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NhapMa - UI Helper
 // @namespace    nhapma-helper
-// @version      1.0
+// @version      1.3
 // @description  Semi-auto human-interaction for nhapma.com
 // @author       Friczh
 // @match        *://*/*
